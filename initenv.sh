891f6e1c4355e95c41e38fa376364064bd9a5ca3
Mark Sobkow <mark.sobkow@gmail.com>
2026-06-21 12:05:32 -0600

    2026-06-21 12:00 CST MSS Code Factory alpha06 released at https://github.com/msobkow/org.msscf.msscf.v2_13-installer/releases/tag/alpha06

--- initenv.sh
+++ initenv.sh
@@ -6,5 +6,5 @@ if [ ! -d /opt/msscode ]; then
 fi
 export INSTDIR=`pwd`
 cd /opt/msscode
-export RELEASE_VERSION="org.msscf.msscf.v2_13-alpha05"
-export SNAPSHOT_VERSION="snapshot-202606171400"
+export RELEASE_VERSION="org.msscf.msscf.v2_13-alpha06"
+export SNAPSHOT_VERSION="snapshot-202606211150"
--- upgrade-kbase.sh
+++ upgrade-kbase.sh
@@ -6,7 +6,7 @@ if [ ! -d /opt/msscode ]; then
 fi
 export INSTDIR=`pwd`
 cd /opt/msscode
-export SNAPSHOT_VERSION="snapshot-202606171400"
+export SNAPSHOT_VERSION="snapshot-202606211150"
 # Install the new knowledge base
 tar xfz $INSTDIR/org.msscf.msscf.v2_13.cfkbase-${SNAPSHOT_VERSION}.tar.gz
 if [ -L /opt/msscode/org.msscf.msscf.v2_13.cfkbase ]; then
