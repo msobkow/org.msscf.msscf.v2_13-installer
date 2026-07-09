0535bb957a2a1fa490609124067ce26b889a7d3a
Mark Sobkow <mark.sobkow@gmail.com>
2026-07-09 04:33:46 -0600

    MSS Code Factory 2.13 alpha08 is now available at https://github.com/msobkow/org.msscf.msscf.v2_13-installer/releases/tag/alpha08. You can find installation instructions at https://github.com/msobkow/org.msscf.msscf.v2_13-installer/ and the README.md displayed there. There have also been releases of the 2.13 knowledge base and model set as of 2026-07-09 03:45 CST, tagged as snapshot-202607090345 which are included in the installer and are required alongside the executable jars to run the tool. Alpha08 fleshes out use of the CodeVis attributes to produce pub and prot interfaces as specified by CodeVis, but the next step of integrating the new interfaces with the private implementation will take some time, and then the private implementations need to be propagated to pub and prot implementations that prune out functionality rather than adding it in painful increments as I did for building up the interface layers so far. But at least this Alpha produces clean-compiling 3.1 code, unlike the previous alpha. There have been some minor changes to the binaries as well, specifically built in verbs and debugging of same.

--- initenv.sh
+++ initenv.sh
@@ -6,5 +6,5 @@ if [ ! -d /opt/msscode ]; then
 fi
 export INSTDIR=`pwd`
 cd /opt/msscode
-export RELEASE_VERSION="org.msscf.msscf.v2_13-alpha07"
-export SNAPSHOT_VERSION="snapshot-202606301000"
+export RELEASE_VERSION="org.msscf.msscf.v2_13-alpha08"
+export SNAPSHOT_VERSION="snapshot-202607090345"
