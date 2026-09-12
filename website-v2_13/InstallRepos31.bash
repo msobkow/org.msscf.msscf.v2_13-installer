4602c2f7c2246e0956c044082e3a2d2ad473de56
Mark Sobkow <mark.sobkow@gmail.com>
2026-09-12 05:43:01 -0600

    2026-09-11 20:50 CST This is the big one - the code produced has been completely restructured.

--- website-v2_13/InstallRepos31.bash
+++ website-v2_13/InstallRepos31.bash
@@ -19,49 +19,10 @@ if [ "$MCF_HOME" != "" ]; then
 		server.markhome.mcf \
 		server.markhome.mcf.v3_1 \
 		server.markhome.mcf.v3_1.cflib \
-		server.markhome.mcf.v3_1.cflib.dbutil \
-		server.markhome.mcf.v3_1.cflib.dbtest \
-		server.markhome.mcf.v3_1.cflib.javafx \
-		server.markhome.mcf.v3_1.cfsec.cfsecpub \
-		server.markhome.mcf.v3_1.cfsec.cfsecprot \
-		server.markhome.mcf.v3_1.cfsec.cfsec \
-		server.markhome.mcf.v3_1.cfsec.cfsecbuff \
-		server.markhome.mcf.v3_1.cfsec.cfsecjpa \
-		server.markhome.mcf.v3_1.cfsec.cfsecsaxloader \
-		server.markhome.mcf.v3_1.cfsec.cfsecram \
-		server.markhome.mcf.v3_1.cfsec.cfsecjpatest \
-		server.markhome.mcf.v3_1.cfsec.cfsecramtest \
-		server.markhome.mcf.v3_1.cfsec.cfsecjavafx \
-		server.markhome.mcf.v3_1.cfsec.cfseccustfx \
-		server.markhome.mcf.v3_1.cfsec.cfsecappsetup \
-		server.markhome.mcf.v3_1.cfsec.cfsecsaxramldr \
-		server.markhome.mcf.v3_1.cfint.cfintpub \
-		server.markhome.mcf.v3_1.cfint.cfintprot \
-		server.markhome.mcf.v3_1.cfint.cfint \
-		server.markhome.mcf.v3_1.cfint.cfintbuff \
-		server.markhome.mcf.v3_1.cfint.cfintjpa \
-		server.markhome.mcf.v3_1.cfint.cfintsaxloader \
-		server.markhome.mcf.v3_1.cfint.cfintram \
-		server.markhome.mcf.v3_1.cfint.cfintjpatest \
-		server.markhome.mcf.v3_1.cfint.cfintramtest \
-		server.markhome.mcf.v3_1.cfint.cfintjavafx \
-		server.markhome.mcf.v3_1.cfint.cfintcustfx \
-		server.markhome.mcf.v3_1.cfint.cfintappsetup \
-		server.markhome.mcf.v3_1.cfint.cfintsaxramldr \
-		server.markhome.mcf.v3_1.cfbam.cfbampub \
-		server.markhome.mcf.v3_1.cfbam.cfbamprot \
-		server.markhome.mcf.v3_1.cfbam.cfbam \
-		server.markhome.mcf.v3_1.cfbam.cfbambuff \
-		server.markhome.mcf.v3_1.cfbam.cfbamjpa \
-		server.markhome.mcf.v3_1.cfbam.cfbamsaxloader \
-		server.markhome.mcf.v3_1.cfbam.cfbamram \
-		server.markhome.mcf.v3_1.cfbam.cfbamjpatest \
-		server.markhome.mcf.v3_1.cfbam.cfbamramtest \
-		server.markhome.mcf.v3_1.cfbam.cfbamjavafx \
-		server.markhome.mcf.v3_1.cfbam.cfbamcustfx \
-		server.markhome.mcf.v3_1.cfbam.cfbamcusteditor \
-		server.markhome.mcf.v3_1.cfbam.cfbamappsetup \
-		server.markhome.mcf.v3_1.cfbam.cfbamsaxramldr
+		server.markhome.mcf.v3_1.cfsec \
+		server.markhome.mcf.v3_1.cfint \
+		server.markhome.mcf.v3_1.cfbam \
+		server.markhome.mcf.v3_1.cfexec
 	do
 		if [ ! -d $arepo ]; then
 			git clone git@github.com:msobkow/${arepo}.git
