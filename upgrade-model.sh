3b6d9e4911b6a2b04dea0bc44948b5029e745b69
Mark Sobkow <mark.sobkow@gmail.com>
2026-09-12 03:44:42 -0600

    2026-09-12 03:45 CST MSS Code Factory alpha11 released at https://github.com/msobkow/org.msscf.msscf.v2_13-installer/releases/tag/alpha11

--- upgrade-model.sh
+++ upgrade-model.sh
@@ -6,7 +6,7 @@ if [ ! -d /opt/msscode ]; then
 fi
 export INSTDIR=`pwd`
 cd /opt/msscode
-export SNAPSHOT_VERSION="snapshot-202608190345"
+export SNAPSHOT_VERSION="snapshot-202609112100"
 #	Install the new models
 tar xfz $INSTDIR/org.msscf.msscf.v2_13.cfmodel-${SNAPSHOT_VERSION}.tar.gz
 if [ -L /opt/msscode/org.msscf.msscf.v2_13.cfmodel ]; then
